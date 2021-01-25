8479c3d529c35b421b834637df38b6aa86ab482e
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-01-25 19:55:07 +0800

    feat: schema reset

--- models/restaurant.js
+++ models/restaurant.js
@@ -7,24 +7,30 @@ const restaurantSchema = new Schema({
   },
   category: {
     type: String,
+    required: true,
   },
   image: {
     type: String,
   },
   address: {
     type: String,
+    required: true,
   },
   phone: {
     type: String,
+    required: true,
   },
   google_map: {
     type: String,
+    required: true,
   },
   rating: {
     type: Number,
+    required: true,
   },
   description: {
     type: String,
+    required: true,
   },
   menu: {
     type: String,
