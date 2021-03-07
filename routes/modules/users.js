d6cc892ff20198e4571500f8e15e448c80fd7c68
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-03-07 19:38:28 +0800

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -19,6 +20,7 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const registration_success_msg = `${email}會員註冊成功！`
   const errors = []
   if (!email || !password || !confirmPassword) {
     errors.push({ message: '請填寫信箱與密碼欄位。' })
@@ -26,30 +28,6 @@ router.post('/register', (req, res) => {
   if (password !== confirmPassword) {
     errors.push({ message: '密碼與確認密碼不相符！' })
   }
-
-  User.findOne({ email }).then(user => {
-    const registration_success_msg = `${email}會員註冊成功！`
-    if (user) {
-      errors.push({ message: '此會員信箱已註冊！' })
-      res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    } else {
-      return User.create({
-        name,
-        email,
-        password
-      })
-        .then(() => res.render('login', {
-          email,
-          registration_success_msg
-        }))
-        .catch(err => console.log(err))
-    }
-  })
   if (errors.length) {
     return res.render('register', {
       errors,
@@ -59,6 +37,31 @@ router.post('/register', (req, res) => {
       confirmPassword
     })
   }
+
+  User.findOne({ email }).then(user => {
+    if (user) {
+      errors.push({ message: '此會員信箱已註冊！' })
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+  })
+    .then(() => res.render('login', {
+      email,
+      registration_success_msg
+    }))
+    .catch(err => console.log(err))
 })
 
 router.get('/logout', (req, res) => {
