41e584c5f0a8b635cd5330f3275f9e54e3d113cd
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-03-08 19:38:41 +0800

    feat: modfiy index hbs & sort seletions

--- routes/modules/sorts.js
+++ routes/modules/sorts.js
@@ -5,10 +5,18 @@ const Restaurant = require('../../models/restaurant')
 router.get('/', (req, res) => {
   const userId = req.user._id
   const sorts = req.query.sorts
+  const selection = [
+    { value: 'name', selected: '名稱 A -> Z' },
+    { value: '-name', selected: '名稱 Z -> A' },
+    { value: 'category', selected: '類別 A -> Z' },
+    { value: '-category', selected: '類別 Z -> A' },
+    { value: '-rating', selected: '評比高至低' },
+    { value: 'rating', selected: '評比低至高' },]
+  const currentSelected = selection.find(selection => sorts === selection.value).selected
   Restaurant.find({ userId })
     .lean()
     .sort(sorts)
-    .then(restaurants => res.render('index', { restaurants, sorts }))
+    .then(restaurants => res.render('index', { restaurants, sorts, currentSelected }))
     .catch(error => console.log(error))
 })
 
